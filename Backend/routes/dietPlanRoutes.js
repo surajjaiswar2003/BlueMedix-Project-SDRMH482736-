4ed75a7ba06608c5f4912997279a149b82f81feb
utsabnandi <utsab22nandi@gmail.com>
2025-05-09 18:41:11 +0530

    Admin Dashboard Fixed

--- Backend/routes/dietPlanRoutes.js
+++ Backend/routes/dietPlanRoutes.js
@@ -10,8 +10,8 @@ router.get("/current/:userId", dietPlanController.getCurrentDietPlan);
 // --- DIETITIAN ENDPOINTS ---
 router.get("/review", dietPlanController.getPlansForReview);
 router.get("/", dietPlanController.getDietPlans); // <-- ADD THIS LINE
-router.get("/:id", dietPlanController.getDietPlanDetails);
 router.get("/count", dietPlanController.getDietPlanCount);
+router.get("/:id", dietPlanController.getDietPlanDetails);
 router.put("/:id/meal", dietPlanController.updateMealInDietPlan);
 router.put("/:id/approve", dietPlanController.approveDietPlan);
 
